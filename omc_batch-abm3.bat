5ffd0bbff6fc5a0cb5a17cc24732673193738ddb
jchu-sandag <97865624+jchu-sandag@users.noreply.github.com>
2025-01-03 10:13:23 -0800

    Edit batch file to take in spaces in directory names

--- omc_batch-abm3.bat
+++ omc_batch-abm3.bat
@@ -1,5 +1,5 @@
 ECHO Running carshare carshare calculator
-call python carshare/src/carshare_calculator_ABM3.py config_abm3.yml C:/Regional Plan ABM3 OMC
+call python carshare/src/carshare_calculator_ABM3.py config_abm3.yml "C:/Regional Plan ABM3 OMC"
 
 ECHO Running vanpool carshare calculator
-call python vanpool/src/vanpool_calculator_ABM3.py config_abm3.yml C:/Regional Plan ABM3 OMC
+call python vanpool/src/vanpool_calculator_ABM3.py config_abm3.yml "C:/Regional Plan ABM3 OMC"
